8fed2a7afa361b27f094db8312adf61fdda96aac
Gabriel Cassiano Carvalho Lara <130706226+1GabrielC@users.noreply.github.com>
2025-10-15 17:12:30 -0300

    Delete Trabalho1/one-piece-crud-app directory

--- Trabalho1/one-piece-crud-app/src/utils/constants.ts
+++
@@ -1,4 +0,0 @@
-// Ajuste para onde seu servidor Go está rodando.
-// Ex.: 'http://10.0.2.2:8080' no Android emulador (host da máquina)
-// ou 'http://localhost:8080' no iOS simulador, ou IP da rede local.
-export const API_URL = 'http://10.0.2.2:8080';
